7ceaa3fcc498df213abe8acef9316f542b9bdf14
mizukmb <mizukmb6@gmail.com>
2018-07-02 23:19:03 +0900

    :white_flower: Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate clap;
+use clap::{App};
+
 fn main() {
-    println!("Hello, world!");
+    let name = env!("CARGO_PKG_NAME");
+    let version = env!("CARGO_PKG_VERSION");
+    let author = env!("CARGO_PKG_AUTHORS");
+    let about = "Print today's your esa.io articles";
+    App::new(name)
+        .version(version)
+        .author(author)
+        .about(about)
+        .get_matches();
+
+    let esa_url = "";
 }
