d8df98cc9728caf4b3139dc875eb0beae9ddf754
mizukmb <mizukmb6@gmail.com>
2019-01-02 01:27:31 +0900

    Refactor: Split module each subcommnads

---
+++ src/subcommand.rs
@@ -0,0 +1,25 @@
+use std::io;
+use std::io::Write;
+use super::esa_config;
+
+pub fn init() {
+    let mut team = String::new();
+    let mut screen_name = String::new();
+
+    let parsonal_access_token =
+        rpassword::prompt_password_stdout("Personal access token (hidden): ").unwrap();
+    print!("Team: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut team).unwrap();
+    print!("Screen name: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut screen_name).unwrap();
+
+    let config = esa_config::new((
+        team.trim().to_string(),
+        screen_name.trim().to_string(),
+        parsonal_access_token.trim().to_string(),
+    ));
+
+    config.write();
+}
